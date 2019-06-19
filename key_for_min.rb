d6bf629cc45910694fdf82b09ac4a2966a59af3f
Sergio Guzman <hiserg98@gmail.com>
2019-06-19 01:05:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,11 @@ def key_for_min_value(name_hash)
 num = 0
 name_a = nil
   name_hash.each do |name, number|
-    if number 
+    if num == 0 || number < num
+      num = number
+      name_a = name
     
     end
   end
+  name_a
 end
